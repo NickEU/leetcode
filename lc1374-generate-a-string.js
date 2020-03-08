fa184ebbab838eb093a790b499224b393a308fe7
Nick <neverate@gmail.com>
2020-03-08 22:25:55 +0200

    +lc 1374

---
+++ lc1374-generate-a-string.js
@@ -0,0 +1,18 @@
+// Given an integer n, return a string with n characters
+// such that each character in such string occurs an odd number of times.
+
+// The returned string must contain only lowercase English letters.
+// If there are multiples valid strings, return any of them.
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+
+var generateTheString = function(n) {
+  let result = Array(n).fill("a");
+  if (n % 2 === 0) {
+    result[0] = "b";
+  }
+  return result;
+};
