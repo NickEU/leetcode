57f78405ba2348ca47c70150d144f8d2e26621e7
Nick <neverate@gmail.com>
2020-05-03 22:15:33 +0300

    #0009 upd eslint

--- lc0009-1-palindrome-number.js
+++ lc0009-1-palindrome-number.js
@@ -1,17 +1,21 @@
+'use strict';
+// Determine whether an integer is a palindrome.
+// An integer is a palindrome when
+// it reads the same backward as forward.
+
 /**
  * @param {number} x
  * @return {boolean}
  */
 // solved using integers, without converting to a string
-var isPalindrome = function(x) {
+const isPalindrome = function(x) {
   if ((x % 10 === 0 && x !== 0) || x < 0) {
     return false;
   }
   let temp = 0;
-  let start = x;
+  const start = x;
 
   while (x > 0) {
-    console.log(temp);
     if (temp !== 0) {
       temp *= 10;
     }
@@ -19,5 +23,9 @@ var isPalindrome = function(x) {
     x = Math.floor(x / 10);
   }
 
-  return temp === start ? true : false;
+  return temp === start;
 };
+
+console.log(isPalindrome(121) === true);
+console.log(isPalindrome(-121) === false);
+console.log(isPalindrome(10) === false);
--- lc0009-2-palindrome-number.js
+++ lc0009-2-palindrome-number.js
@@ -1,15 +1,20 @@
+'use strict';
+// Determine whether an integer is a palindrome.
+// An integer is a palindrome when
+// it reads the same backward as forward.
+
 /**
  * @param {number} x
  * @return {boolean}
  */
 // solved via conversion to a string
-var isPalindrome = function(x) {
+const isPalindrome = function(x) {
   if ((x % 10 === 0 && x !== 0) || x < 0) {
     return false;
   }
 
-  let xStr = x.toString();
-  let lastElIndex = xStr.length - 1;
+  const xStr = x.toString();
+  const lastElIndex = xStr.length - 1;
 
   for (let i = 0; i < lastElIndex - i; i++) {
     if (xStr[i] !== xStr[lastElIndex - i]) {
@@ -19,3 +24,7 @@ var isPalindrome = function(x) {
 
   return true;
 };
+
+console.log(isPalindrome(121) === true);
+console.log(isPalindrome(-121) === false);
+console.log(isPalindrome(10) === false);
