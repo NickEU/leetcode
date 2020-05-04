67328529040d073d859d7fee252a84162722a28c
Nick <neverate@gmail.com>
2020-05-04 19:57:10 +0300

    #0014 upd eslint

--- lc0014-1-longest-common-prefix.js
+++ lc0014-1-longest-common-prefix.js
@@ -1,16 +1,23 @@
+'use strict';
+// Write a function to find the longest
+// common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
 /**
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-  let result = "";
+
+const longestCommonPrefix = function(strs) {
+  let result = '';
 
   if (strs.length === 0) {
-    return "";
+    return '';
   }
 
   for (let j = 0; strs[0][j] !== undefined; j++) {
-    let currChar = strs[0][j];
+    const currChar = strs[0][j];
     let charIsCommon = true;
     for (let i = 1; i < strs.length; i++) {
       if (currChar !== strs[i][j]) {
@@ -28,3 +35,7 @@ var longestCommonPrefix = function(strs) {
 
   return result;
 };
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight']) === 'fl');
+console.log(longestCommonPrefix(['dog', 'racecar', 'car']) === '');
+console.log(longestCommonPrefix(['sadist', 'sad']) === 'sad');
