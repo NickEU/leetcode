39bfb1be3be607b454a7c659ea5c2a094b54c839
Nick <neverate@gmail.com>
2020-05-08 12:16:53 +0300

    #1437 + solution

---
+++ lc1437-els-in-arrays-spaced-out.js
@@ -0,0 +1,31 @@
+'use strict';
+//Given an array nums of 0s and 1s and an integer k,
+//return True if all 1's are at least k places away
+//from each other, otherwise return False.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+
+const kLengthApart = function(nums, k) {
+  let currDistance;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      if (currDistance < k) {
+        return false;
+      } else {
+        currDistance = 0;
+      }
+    } else {
+      currDistance++;
+    }
+  }
+  return true;
+};
+
+console.log(kLengthApart([1, 0, 0, 0, 1, 0, 0, 1], 2) === true);
+console.log(kLengthApart([1, 0, 0, 1, 0, 1], 2) === false);
+console.log(kLengthApart([1, 1, 1, 1, 1], 0) === true);
+console.log(kLengthApart([0, 1, 0, 1], 1) === true);
