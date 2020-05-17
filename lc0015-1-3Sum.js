7516a89d4039093b3b1c07eccb8d24ad250a8c64
Nick <neverate@gmail.com>
2020-05-17 17:27:22 +0300

    #0015 upd (-iliffe vector +set) +cleaner testing

--- lc0015-1-3Sum.js
+++ lc0015-1-3Sum.js
@@ -1,9 +1,15 @@
 'use strict';
+// Given an array nums of n integers, are there
+// elements a, b, c in nums such that a + b + c = 0?
+// Find all unique triplets in the array which gives the sum of zero.
+// The solution set must not contain duplicate triplets.
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
-
+// replaced an iliffe vector with a set:
+// a far superior option for dealing with duplicates.
 const twoSum = function(result, arr, targetIdx) {
   const targetSum = -arr[targetIdx];
   const hashT = {};
@@ -15,9 +21,7 @@ const twoSum = function(result, arr, targetIdx) {
     if (hashT[currEl] === true) {
       const potentialTriplet =
       [-targetSum, targetSum - currEl, currEl].sort((a, b) => a - b);
-      if (tripletExistsAndNotDuplicate(result, potentialTriplet)) {
-        result.push(potentialTriplet);
-      }
+      result.add(JSON.stringify(potentialTriplet));
     } else {
       hashT[targetSum - currEl] = true;
     }
@@ -27,36 +31,35 @@ const twoSum = function(result, arr, targetIdx) {
 };
 
 const threeSum = function(nums) {
-  const result = [];
+  const setOfTriplets = new Set();
   for (let i = 0; i < nums.length; i++) {
-    twoSum(result, nums, i);
+    twoSum(setOfTriplets, nums, i);
   }
-  return result;
+  const result = Array.from(setOfTriplets);
+  return result.map(el => JSON.parse(el));
 };
 
-function tripletExistsAndNotDuplicate(arr, potentialTriplet) {
-  if (!Array.isArray(potentialTriplet)) {
-    return false;
-  }
-  for (let i = 0; i < arr.length; i++) {
-    const currTriplet = arr[i];
-    if (currTriplet[0] === potentialTriplet[0] &&
-    currTriplet[1] === potentialTriplet[1] &&
-    currTriplet[2] === potentialTriplet[2]) {
-      return false;
-    }
-  }
 
-  return true;
+let expected = JSON.stringify([]);
+let actual = JSON.stringify(threeSum([0, 0]));
+test(actual, expected);
+
+expected = JSON.stringify([ [ -1, 0, 1 ], [ -1, -1, 2 ] ]);
+actual = JSON.stringify(threeSum([-1, 0, 1, 2, -1, -4]));
+test(actual, expected);
+
+const inputArr = [-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6];
+expected = JSON.stringify([
+  [ -4, 2, 2 ],
+  [ -4, 1, 3 ],
+  [ -4, 0, 4 ],
+  [ -4, -2, 6 ],
+  [ -2, 0, 2 ],
+  [ -2, -2, 4 ]
+]);
+actual = JSON.stringify(threeSum(inputArr));
+test(actual, expected);
+
+function test(actual, expected) {
+  console.log(actual === expected);
 }
-
-//console.log(twoSum([2, -2, 4, -5, 7], 3));
-//console.log(threeSum([0, 0]));
-//console.log(threeSum([-1, 0, 1, 2, -1, -4]));
-//console.log(threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]));
-//console.log(threeSum(
-//[-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0]));
-const arr = [-7, -5, 5, -6, -2, 1, 7, 3,
-  -4, -2, -2, -4, -8, -1, 8, 8,
-  -2, -7, 3, 2, -7, 8, -3, -10, 5, 2, 8, 7, 7];
-console.log(threeSum(arr));
