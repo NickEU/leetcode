e27ef6c284c4a7f3ac8589690eb1b5d225f9ff0b
Nick <neverate@gmail.com>
2020-06-02 10:45:59 +0300

    #1464 upd code formatting

--- LC1464_1.java
+++ LC1464_1.java
@@ -1,19 +1,19 @@
 class LC1464_1 {
-    public int maxProduct(int[] nums) {
-        int max = nums[0] > nums[1] ? nums[0] : nums[1];
-        int maxSecond = max == nums[0] ? nums[1] : nums[0];
-        
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] > maxSecond) {
-                if (nums[i] > max) {
-                    maxSecond = max;
-                    max = nums[i];
-                } else {
-                    maxSecond = nums[i];
-                }          
-            }
+  public int maxProduct(int[] nums) {
+    int max = Math.max(nums[0], nums[1]);
+    int maxSecond = max == nums[0] ? nums[1] : nums[0];
+
+    for (int i = 2; i < nums.length; i++) {
+      if (nums[i] > maxSecond) {
+        if (nums[i] > max) {
+          maxSecond = max;
+          max = nums[i];
+        } else {
+          maxSecond = nums[i];
         }
-        
-        return (max - 1) * (maxSecond - 1);
+      }
     }
+
+    return (max - 1) * (maxSecond - 1);
+  }
 }
--- LC1464_2.java
+++ LC1464_2.java
@@ -1,20 +1,18 @@
 public class LC1464_2 {
-    public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE,
-            maxSecond = max;
-        
-        for (int number : nums) {
-            if (number > maxSecond) {
-                if (number > max) {
-                    maxSecond = max;
-                    max = number;
-                } else {
-                    maxSecond = number;
-                }          
-            }
+  public int maxProduct(int[] nums) {
+    int max = Integer.MIN_VALUE, maxSecond = max;
+
+    for (int number : nums) {
+      if (number > maxSecond) {
+        if (number > max) {
+          maxSecond = max;
+          max = number;
+        } else {
+          maxSecond = number;
         }
-        
-        return (max - 1) * (maxSecond - 1);
+      }
     }
- 
+
+    return (max - 1) * (maxSecond - 1);
+  }
 }
