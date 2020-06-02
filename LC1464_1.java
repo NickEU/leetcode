c57c7ec729b1c942416b14f69fd5599782b8299a
Nick <neverate@gmail.com>
2020-06-02 10:21:36 +0300

    #1464 + solution

---
+++ LC1464_1.java
@@ -0,0 +1,19 @@
+class LC1464_1 {
+    public int maxProduct(int[] nums) {
+        int max = nums[0] > nums[1] ? nums[0] : nums[1];
+        int maxSecond = max == nums[0] ? nums[1] : nums[0];
+        
+        for (int i = 2; i < nums.length; i++) {
+          if (nums[i] > maxSecond) {
+            if (nums[i] > max) {
+              maxSecond = max;
+              max = nums[i];
+            } else {
+              maxSecond = nums[i];
+            }          
+          }
+        }
+        
+        return (max - 1) * (maxSecond - 1);
+    }
+}
---
+++ LC1464_2.java
@@ -0,0 +1,20 @@
+public class LC1464_2 {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE,
+            maxSecond = max;
+        
+        for (int number : nums) {
+        if (number > maxSecond) {
+            if (number > max) {
+            maxSecond = max;
+            max = number;
+            } else {
+            maxSecond = number;
+            }          
+        }
+        }
+        
+        return (max - 1) * (maxSecond - 1);
+    }
+ 
+}
