c1cf01495770ff8d64d849c025b025fdf2fc0f77
Nick <neverate@gmail.com>
2020-06-02 10:46:25 +0300

    #0704 upd code formatting

---
+++ LC0704_3_binary_search.java
@@ -0,0 +1,22 @@
+class LC0704_3_binary_search {
+  public int search(int[] nums, int target) {
+    int foundId = -1;
+    int startIdx = 0;
+    int endIdx = nums.length - 1;
+    while (startIdx <= endIdx) {
+      int midIdx = startIdx + (endIdx - startIdx) / 2;
+      int currId = nums[midIdx];
+      if (currId == target) {
+        foundId = midIdx;
+        break;
+      }
+
+      if (currId > target) {
+        endIdx = midIdx - 1;
+      } else {
+        startIdx = midIdx + 1;
+      }
+    }
+    return foundId;
+  }
+}
