c0813b2647294b32ed9c4d2b1546633208a19990
Nick <neverate@gmail.com>
2020-06-03 11:02:13 +0300

    #0003 upd eslint

--- lc0003-1-longest-substring.js
+++ lc0003-1-longest-substring.js
@@ -1,20 +1,21 @@
+'use strict';
+
+// Given a string, find the length of the longest substring
+// without repeating characters.
+
 /**
  * @param {string} s
  * @return {number}
  */
 
 // bad brute force solution, nested loops galore, O(n^2)
-//  Runtime: 284 ms, faster than 23.98% of JavaScript online submissions for Longest Substring Without Repeating Characters.
-//  Memory Usage: 42.1 MB, less than 23.01% of JavaScript online submissions for Longest Substring Without Repeating Characters.
-
-var lengthOfLongestSubstring = function(s) {
+const lengthOfLongestSubstring = function(s) {
   let result = 0;
   for (let i = 0; i < s.length; i++) {
-    let setOfChars = new Set([s[i]]);
+    const setOfChars = new Set([s[i]]);
     let currLongest = 1;
     for (let j = i + 1; j < s.length; j++) {
       if (setOfChars.has(s[j])) {
-        //console.log(`duplicate ${s[j]}, l = ${currLongest}`);
         break;
       }
       setOfChars.add(s[j]);
@@ -28,3 +29,7 @@ var lengthOfLongestSubstring = function(s) {
 
   return result;
 };
+
+console.log(lengthOfLongestSubstring('abcabcbb') === 3);
+console.log(lengthOfLongestSubstring('bbbbb') === 1);
+console.log(lengthOfLongestSubstring('pwwkew') === 3);
