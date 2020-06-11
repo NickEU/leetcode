6cb432acdfc131b7c8ff18fe3e8b530b4510eae6
Nick <neverate@gmail.com>
2020-06-11 14:39:54 +0300

    #1460 upd

--- lc1460-1-can-make-two-arrays-equal.js
+++ lc1460-1-can-make-two-arrays-equal.js
@@ -37,8 +37,8 @@ const canBeEqual = function(target, arr) {
   return canBeEqual;
 };
 
-console.log(canBeEqual([1, 2, 3, 4], [2, 4, 1, 3]) === true);
-console.log(canBeEqual([7], [7]) === true);
-console.log(canBeEqual([1, 12], [12, 1]) === true);
-console.log(canBeEqual([3, 7, 9], [3, 7, 11]) === false);
-console.log(canBeEqual([1, 1, 1, 1, 1], [1, 1, 1, 1, 1]) === true);
+console.log(canBeEqual([1, 2, 3, 4], [2, 4, 1, 3]));
+console.log(canBeEqual([7], [7]));
+console.log(canBeEqual([1, 12], [12, 1]));
+console.log(!canBeEqual([3, 7, 9], [3, 7, 11]));
+console.log(canBeEqual([1, 1, 1, 1, 1], [1, 1, 1, 1, 1]));
