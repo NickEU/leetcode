4fcf3ff0edfb9ae40bf08f57d904ed8ed3d074d0
Nick <neverate@gmail.com>
2020-06-11 14:41:40 +0300

    #1437 upd

--- LC1437_1_els_in_arrays_spaced_out.js
+++ LC1437_1_els_in_arrays_spaced_out.js
@@ -25,7 +25,7 @@ const kLengthApart = function(nums, k) {
   return true;
 };
 
-console.log(kLengthApart([1, 0, 0, 0, 1, 0, 0, 1], 2) === true);
-console.log(kLengthApart([1, 0, 0, 1, 0, 1], 2) === false);
-console.log(kLengthApart([1, 1, 1, 1, 1], 0) === true);
-console.log(kLengthApart([0, 1, 0, 1], 1) === true);
+console.log(kLengthApart([1, 0, 0, 0, 1, 0, 0, 1], 2));
+console.log(!kLengthApart([1, 0, 0, 1, 0, 1], 2));
+console.log(kLengthApart([1, 1, 1, 1, 1], 0));
+console.log(kLengthApart([0, 1, 0, 1], 1));
